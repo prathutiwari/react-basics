57c415c5a23fc923a37f980b57c751c262eb59d6
prathutiwari <prathutiwari22@gmail.com>
2025-02-13 16:34:16 +0530

    Episode - 06

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,16 +4,26 @@ import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [filterRestaurant, setFilterRestaurant] = useState(resList)
+  const [searchText, setSearchText] = useState('')
+
 
   return (
     <div className="body">
       <div className="filter">
+        <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+        <button onClick={() => {
+          const filterData = listOfRestaurant.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+          setFilterRestaurant(filterData)
+        }}>Search</button>
+
+
         <button
           className="filterBtn"
           onClick={() => {
             // Filter logic
             const filteredRestaurant = resList.filter((res) => res?.info?.avgRating > 4.3);
-            setListOfRestaurant(filteredRestaurant)
+            setFilterRestaurant(filteredRestaurant)
           }}
         >
           Top Rated Restaurant
@@ -21,14 +31,15 @@ const Body = () => {
         <button
           className="filterBtn"
           onClick={() => {
-            setListOfRestaurant(resList)
+            setFilterRestaurant(resList)
           }}
         >
           All Restaurant
         </button>
+
       </div>
       <div className="res-container">
-        {listOfRestaurant.map((restaurant) => (
+        {filterRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
